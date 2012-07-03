e8887531b413e35170ea75d41e83edeef347bf81
Ken Warner <kjwarner@gmail.com>
2012-07-03 17:11:09 -0400

    initial commit

---
+++ FooTimestamp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FooTimestamp
+{
+	public class Foo
+	{
+		public DateTime Timestamp { get; set; }
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Foo> transactions = new List<Foo>() { new Foo() { Timestamp = new DateTime(2012, 7, 1) } };
+			var maxTimestamp = transactions.Any() ? transactions.Max(x => x.Timestamp) : new DateTime(2012, 7, 2);
+		} // put a breakpoint here
+	}
+}
